686f7cb3be125651a04dc2061aec5b258b7ecada
Георгий Гостев <geragostev@gmail.com>
2021-12-13 04:00:52 +0300

    Added /help and current date for get request

--- handlers/parser.py
+++ handlers/parser.py
@@ -1,23 +1,48 @@
 import requests
+import datetime
+
+now = datetime.datetime.now()
 
 
 def getData():
     orders = 0
     total = 0
+    wbtimeformatted = str(now.year) + '-' + str(now.month) + '-' + str(now.day) + 'T00:30:00.000Z'
 
-    response = requests.get(
-        "https://suppliers-stats.wildberries.ru/api/v1/supplier/orders?dateFrom=2021-12-12T08:00:00.000Z&flag=1&key=ODQ0MmFhMzgtMDg2Zi00NzFhLWE0NTMtY2RmMTk0Yzk4ZjIy")
-    data = response.json()
-    if response.status_code == 429:
-        print("Too man request")
+    try:
+        response = requests.get('https://suppliers-stats.wildberries.ru/api/v1/supplier/orders',
+                                params={
+                                    'dateFrom': wbtimeformatted,
+                                    'flag': 1,
+                                    'key': 'ODQ0MmFhMzgtMDg2Zi00NzFhLWE0NTMtY2RmMTk0Yzk4ZjIy'
+                                })
+        data = response.json()
+        status_code = response.status_code
 
-    for order in data:
-        orders += 1
-        price = int(order["totalPrice"]) * int(order["discountPercent"]) / 100
-        total += price
+        if status_code == 200:
+            for order in data:
+                orders += 1
+                price = int(order["totalPrice"]) * int(order["discountPercent"]) / 100
+                total += price
+            answer = "Статистика на " + str(now.day) + "-" + str(now.month) + "-" + str(now.year) \
+                     + "\nКоличество заказов за день: " + str(orders) \
+                     + "\nСумма заказов за день: " + str(total) + 'руб.'
+            print(answer)
+            return answer
 
-    print("Количество заказов за день: " + str(orders))
-    print("Сумма заказов за день: " + str(total) + 'руб.')
+        elif status_code == 429:
+            answer = "Превышено количество допустимых запросов, чутка подожди"
+            print(answer)
+            return answer
 
-    answer = "Количество заказов за день: " + str(orders) + "\n" + "Сумма заказов за день: " + str(total) + 'руб.'
-    return answer
+        else:
+            answer = "У ВБ очередная беда с башкой, апишка не бом-бом" \
+                     "\nstatus code = " + str(response.status_code)
+            print(answer)
+            return answer
+
+    except requests.exceptions.RequestException as e:
+        print(e)
+        return "Какая-то ебейшая ошибка, если не пройдет спустя 5 минут - чекни логи хироку"
+
+getData()
--- handlers/personal_actions.py
+++ handlers/personal_actions.py
@@ -3,11 +3,53 @@ from dispatcher import dp
 from handlers.parser import getData
 
 
+@dp.message_handler(commands=["start"])
+async def return_id_command(message: types.Message):
+    try:
+        answer = "Приветствую, Биджо, я бот-темщик, вот че могу и умею:\n" \
+                 "/help - почитать список команд и прочую поеботу\n" \
+                 "/getId - получить id твоего аккаунта (для фильтрации доступа)\n" \
+                 "/getOrders - агрегированная информация о заказах на сегодня (сумма+количество)\n" \
+                 "/getOrdersDetailed (на этапе разработки) - детализированная информация по заказам\n" \
+                 "Пока больше ничего не умею(("
+        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        buttons = ["/getOrders", "/getId", "/help"]
+        keyboard.add(*buttons)
+        await message.answer(answer, reply_markup=keyboard)
+    except:
+        await message.answer("Биджо-бот болеет, если скоро не выздоровеет - чекайте логи")
+
+
 @dp.message_handler(commands=["getId"])
 async def return_id_command(message: types.Message):
-    await message.answer(message.from_user.id)
+    try:
+        await message.answer(message.from_user.id)
+    except:
+        await message.answer("Биджо-бот болеет, если скоро не выздоровеет - чекайте логи")
 
 
 @dp.message_handler(commands=["getOrders"])
 async def return_id_command(message: types.Message):
-    await message.answer(getData())
+    try:
+        await message.answer(getData())
+    except:
+        await message.answer("Биджо-бот болеет, если скоро не выздоровеет - чекайте логи")
+
+
+# @dp.message_handler(commands=["getOrdersDetailed"])
+# async def return_id_command(message: types.Message):
+#     await message.answer(getData())
+
+
+@dp.message_handler(commands=["help"])
+async def return_id_command(message: types.Message):
+    try:
+        answer = "Ещё раз привет, походу ты забыл хитровыебанные названия команд, лови список:\n" \
+                 "/help - почитать список команд и прочую поеботу\n" \
+                 "/getId - получить id твоего аккаунта (для фильтрации доступа)\n" \
+                 "/getOrders - агрегированная информация о заказах на сегодня (сумма+количество)\n" \
+                 "/getOrdersDetailed (на этапе разработки) - детализированная информация по заказам\n" \
+                 "По всем вопросам: @Georgiums"
+        await message.answer(answer)
+    except:
+        await message.answer("Биджо-бот болеет, если скоро не выздоровеет - чекайте логи")
